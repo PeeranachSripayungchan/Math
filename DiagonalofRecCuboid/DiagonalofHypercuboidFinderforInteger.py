502d6bf131e28eb8f2341484abd648fe9158bff8
PeeranachSripayungchan <71226323+PeeranachSripayungchan@users.noreply.github.com>
2023-03-17 17:30:02 +0700

    Add files via upload

--- DiagonalofRecCuboid/DiagonalofHypercuboidFinderforInteger.py
+++ DiagonalofRecCuboid/DiagonalofHypercuboidFinderforInteger.py
@@ -1,22 +1,28 @@
 import math
 
 def Hypercuboid():
-    print("Find the Diagonal of Hypercuboid/Hypercube (4D-Cuboid/4D-Cube) that is integer") # also can use for diagonal from rectangle or square
-    n = int(input("Enter the Max Diagonal number that you want to find: ")) 
-    list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    try:
+        print("Find the Diagonal of Hypercuboid/Hypercube (4D-Cuboid/4D-Cube) that is integer") # also can use for diagonal from rectangle or square
+        n = int(input("Enter the Max Diagonal number that you want to find: ")) 
+    except ValueError:
+        print("Input Can't be Character! Try Again!")
+        return Hypercuboid()
+    else:
+        print(" \n")
+        list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    for a in range(0, n, 1):
-        a = a + 1
-        for b in range(0, n, 1):
-            b = b + 1
-            for c in range(0, n, 1):
-                c = c + 1
-                for d in range(0, n, 1):
-                    d = d + 1
-                    e = math.sqrt(a**2 + b**2 + c**2 + d**2)
-                    if e % 10 in list and e <= n:
-                        print("All side of Diagonal of Hypercuboid/Hypercube (4D-Cuboid/4D-Cube) that is in integer is: {}, {}, {}, {}, {}".format(a, b, c, d, e))
-                    else:
-                        pass
+        for a in range(0, n, 1):
+            a = a + 1
+            for b in range(0, n, 1):
+                b = b + 1
+                for c in range(0, n, 1):
+                    c = c + 1
+                    for d in range(0, n, 1):
+                        d = d + 1
+                        e = math.sqrt(a**2 + b**2 + c**2 + d**2)
+                        if e % 10 in list and e <= n:
+                            print("All side of Diagonal of Hypercuboid/Hypercube (4D-Cuboid/4D-Cube) that is in integer is: {}, {}, {}, {}, {}".format(a, b, c, d, e))
+                        else:
+                            pass
 
-    print(" \n")
+        print(" \n")
--- DiagonalofRecCuboid/DiagonalofRectangleFinderforInteger.py
+++ DiagonalofRecCuboid/DiagonalofRectangleFinderforInteger.py
@@ -1,19 +1,25 @@
 import math
 
 def Rectangle():
-    print("Find the Diagonal of Rectangle/Square that is integer") # also can use for diagonal from rectangle or square
-    n = int(input("Enter the Max Diagonal number that you want to find: ")) 
-    list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    try:
+        print("Find the Diagonal of Rectangle/Square that is integer") # also can use for diagonal from rectangle or square
+        n = int(input("Enter the Max Diagonal number that you want to find: ")) 
+    except ValueError:
+        print("Input Can't be Character! Try Again!")
+        return Rectangle()
+    else:
+        print(" \n")
+        list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    for a in range(0, n, 1):
-        a = a + 1
-        for b in range(0, n, 1):
-            b = b + 1
-            c = math.sqrt(a**2 + b**2)
-            if c % 10 in list and c <= n:
-                print("All side of Diagonal of Rectangle/Square that is in integer is: {}, {}, {}".format(a, b, c))
-            else:
-                pass
+        for a in range(0, n, 1):
+            a = a + 1
+            for b in range(0, n, 1):
+                b = b + 1
+                c = math.sqrt(a**2 + b**2)
+                if c % 10 in list and c <= n:
+                    print("All side of Diagonal of Rectangle/Square that is in integer is: {}, {}, {}".format(a, b, c))
+                else:
+                    pass
 
-    print(" \n")
-    
+        print(" \n")
+        
